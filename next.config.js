9e59f7e7428db259cd6349b62c58788e98c419f3
Zakki A Haris <zakyharis33@gmail.com>
2021-10-19 19:27:12 +0700

    add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-const { redirect } = require("next/dist/server/api-utils");
-
 module.exports = {
   path: '/_next/image',
   loader: 'default',
+  redirects: async () => {
+    return [
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/zakki_haris/',
+        permanent: false
+      },
+      {
+        source: '/facebook',
+        destination: 'https://web.facebook.com/zacky.haris.1',
+        permanent: false
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/zakki-abdurrahman-haris-785983118/',
+        permanent: false
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/zakkiharis',
+        permanent: false
+      }
+    ]
+  }
 }
