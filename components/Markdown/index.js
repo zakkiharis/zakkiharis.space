ff60590c031f069cfef91a1fefb963be9e14e7e6
Zakki A Haris <zakyharis33@gmail.com>
2021-10-19 21:23:45 +0700

    add clasess css module

--- components/Markdown/index.js
+++ components/Markdown/index.js
@@ -1,6 +1,6 @@
 import marked from 'marked'
-// import classes from './Markdown-style.module.css'
+import classes from './Markdown-style.module.css'
 
 export default function Markdown({ content }) {
-    return <div dangerouslySetInnerHTML={{ __html: marked(content) }} ></div>
+    return <div className={classes.Markdown} dangerouslySetInnerHTML={{ __html: marked(content) }} ></div>
 }
