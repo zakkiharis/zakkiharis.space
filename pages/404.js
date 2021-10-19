d32da7afc7d14621da8648979bfc859781d1cf37
Zakki A Haris <zakyharis33@gmail.com>
2021-10-19 20:18:31 +0700

    responsive 404

--- pages/404.js
+++ pages/404.js
@@ -2,21 +2,20 @@ import Link from 'next/link'
 
 export default function Custom404() {
     return (
-        <div className="bg-blackGray20 py-8">
-            <div className="font-rubik bg-blackGray40 my-16 mx-36 text-center tracking-widest border border-greenLantern shadow-medium rounded-3xl">
-                <div className="text-404 font-bold text-greenLantern">404</div>
-                <div className="text-3xl font-bold text-greenLantern">OPPS<span className="text-white">!</span> PAGES NOT FOUND</div>
-                <div className="text-lg my-5 text-white">
+        <div className="min-h-screen bg-blackGray20 py-8">
+            <div className="font-rubik bg-blackGray40 my-4 sm:my-16 mx-16 sm:mx-32 md:mx-36 p-1 text-center tracking-widest border border-greenLantern shadow-medium rounded-3xl">
+                <div className="text-404 sm:text-sm404 md:text-lg404 font-bold text-greenLantern">404</div>
+                <div className=" text-xl md:text-3xl font-bold text-greenLantern">OPPS<span className="text-white">!</span> PAGES NOT FOUND</div>
+                <div className=" text-md md:text-lg my-5 text-white">
                     <div>Sorry, the page you&#39;re looking for doesn&#39;t exist.</div>
                     <div>If you think something is broken, report a problem.</div>
                 </div>
-
-                <div>
+                <div className="flex flex-col sm:flex-row items-center ">
                     <Link href="/" passHref>
-                        <button className="bg-blackGray30 hover:bg-greenLantern text-greenLantern hover:text-blackGray30 p-3 m-7 rounded-full">RETURN HOME</button>
+                        <button className="bg-blackGray30 mx-auto hover:bg-greenLantern text-greenLantern hover:text-blackGray30 p-3 mb-7 rounded-full">RETURN HOME</button>
                     </Link>
                     <Link href="/" passHref>
-                        <button className="bg-greenLantern hover:bg-blackGray30 text-blackGray30 hover:text-greenLantern p-3 m-7 rounded-full">REPORT PROBLEM</button>
+                        <button className="bg-greenLantern mx-auto hover:bg-blackGray30 text-blackGray30 hover:text-greenLantern p-3 mb-7 rounded-full">REPORT PROBLEM</button>
                     </Link>
                 </div>
             </div>
