8e233ee637395d9863baa568268e451d20a352e8
Zakki A Haris <zakyharis33@gmail.com>
2021-12-15 14:54:27 +0700

    add spinner

--- hoc/Layout/index.js
+++ hoc/Layout/index.js
@@ -1,9 +1,28 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Container from '@container/ContainerBox'
+import Spinner from '@components/UI/Spinner';
 
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
 
 export default function Layout({ children }) {
+
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    // components did update use hook
+    useEffect(() => {
+        const handleStart = (url) => {
+            url !== router.pathname ? setLoading(true) : setLoading(false);
+        }
+
+        const handleComplete = (url) => setLoading(false);
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleComplete);
+        router.events.on("routeChangeError", handleComplete);
+    }, [router])
 
     return (
         <div className="dark:bg-backgroundBlack">
@@ -14,7 +33,9 @@ export default function Layout({ children }) {
                             <Header />
                         </div>
                         <div className="dark:text-white min-h-screen">
-                            {children}
+                            <div>
+                                {loading ? <Spinner loading={loading} /> : children}
+                            </div>
                         </div>
                         <div className="w-full bottom-0">
                             <Footer />
